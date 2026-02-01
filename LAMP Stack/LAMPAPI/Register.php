10710bf33410117cf6a01c422ae63840e9afaaae
Zachary Trenary <zmichaeltrenary@gmail.com>
2026-02-01 04:59:52 -0500

    Fixed issue of allowing duplicate logins during registration

--- LAMP Stack/LAMPAPI/Register.php
+++ LAMP Stack/LAMPAPI/Register.php
@@ -1,5 +1,5 @@
 <?php
-	$inData = getRequestInfo();
+    $inData = getRequestInfo();
 
     #fields
     $FirstName = $inData["firstName"];
@@ -8,35 +8,49 @@
     $Password = $inData["password"];
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+    {
+        $check = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+        $check->bind_param("s", $Login);
+        $check->execute();
+        $result = $check->get_result();
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        if ($result->num_rows > 0)
+        {
+            returnWithError("Login already exists");
+            $check->close();
+            $conn->close();
+            exit();
+        }
+        $check->close();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+        $stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+        $stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+
+        returnWithError("");
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
 ?>
