db1ae532f52231611e5cab011be83e4bb9eaf50d
Zachary Trenary <zmichaeltrenary@gmail.com>
2026-02-01 05:10:17 -0500

    Previous version was bugged since it updated by UserId instead of Contact ID, endpoint is now fixed

--- LAMP Stack/LAMPAPI/UpdateContact.php
+++ LAMP Stack/LAMPAPI/UpdateContact.php
@@ -1,42 +1,74 @@
 <?php
-	$inData = getRequestInfo();
-    #fields
-    $FirstName = $inData["firstName"];
-    $LastName = $inData["lastName"];
-    $Phone = $inData["phone"];
-    $Email = $inData["email"];
-    $UserID = $inData["userId"];
 
-    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE UserId=?");
-		$stmt->bind_param("ssssi", $FirstName, $LastName, $Phone, $Email, $UserID);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$inData = getRequestInfo();
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$id = $inData["id"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    $check = $conn->prepare("SELECT ID FROM Contacts WHERE ID=?");
+    $check->bind_param("i", $id);
+    $check->execute();
+    $result = $check->get_result();
+
+    if ($result->num_rows == 0)
+    {
+        returnWithError("Contact not found");
+        $check->close();
+        $conn->close();
+        exit();
+    }
+    $check->close();
+
+    $stmt = $conn->prepare("UPDATE Contacts
+                            SET FirstName=?, LastName=?, Phone=?, Email=?, UpdatedAt=NOW()
+                            WHERE ID=?");
+    $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $id);
+
+    if ($stmt->execute())
+    {
+        returnWithInfo("Contact updated successfully");
+    }
+    else
+    {
+        returnWithError("Unable to update contact");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($msg)
+{
+    $retValue = '{"error":"", "message":"' . $msg . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
 ?>
