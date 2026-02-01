1229cdb21f85e44fd908abcc8260e8b5f19dfdef
Zachary Trenary <zmichaeltrenary@gmail.com>
2026-02-01 05:17:45 -0500

    uploaded from domain/terminal

---
+++ LAMP Stack/LAMPAPI/DeleteContact.php
@@ -0,0 +1,67 @@
+<?php
+
+$inData = getRequestInfo();
+$id = $inData["id"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    $check = $conn->prepare("SELECT ID FROM Contacts WHERE ID=?");
+    $check->bind_param("i", $id);
+    $check->execute();
+    $result = $check->get_result();
+
+    if ($result->num_rows == 0)
+    {
+        returnWithError("Contact not found");
+        $check->close();
+        $conn->close();
+        exit();
+    }
+    $check->close();
+
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute())
+    {
+        returnWithInfo("Contact deleted successfully");
+    }
+    else
+    {
+        returnWithError("Unable to delete contact");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($msg)
+{
+    $retValue = '{"error":"", "message":"' . $msg . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
